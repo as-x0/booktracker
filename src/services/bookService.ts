355565a9c9c6b2d82869d74afdd5c20b67812c2a
Anna Schiavo <anna.schiavo2006@gmail.com>
2026-07-17 11:48:52 +0200

    findOrCreateBook

--- src/services/bookService.ts
+++ src/services/bookService.ts
@@ -1,5 +1,6 @@
 import { supabase } from "../supabase/client";
 import type { Book } from "../types/Book";
+import {findOrCreateSeries} from "./seriesService.ts";
 
 export async function getBooks(): Promise<Book[]> {
     const { data, error } = await supabase
@@ -16,4 +17,71 @@ export async function getBooks(): Promise<Book[]> {
     }
 
     return data as Book[];
+}
+
+export async function findOrCreateBook(
+    data: {
+        title: string;
+        authorId: string;
+        genreId: string;
+        publicationYear: number;
+        originalLanguageId: string;
+
+        seriesName?: string;
+        seriesNumber?: number;
+
+        themes?: string;
+    }
+): Promise<string> {
+    let seriesId = null;
+
+    if(data.seriesName){
+        seriesId = await findOrCreateSeries(
+            data.seriesName
+        );
+    }
+
+    const { data: existingBook, error: searchError } = await supabase
+        .from("books")
+        .select("id")
+        .eq("title", data.title)
+        .eq("author_id", data.authorId)
+        .eq(
+            "publication_year",
+            data.publicationYear
+        )
+        .eq(
+            "original_language_id",
+            data.originalLanguageId
+        )
+        .maybeSingle();
+
+    if(searchError){
+        throw searchError;
+    }
+
+    if(existingBook){
+        return existingBook.id;
+    }
+
+    const { data: newBook, error: insertError } = await supabase
+        .from("books")
+        .insert({
+            title: data.title,
+            author_id: data.authorId,
+            genre_id: data.genreId,
+            publication_year: data.publicationYear,
+            original_language_id: data.originalLanguageId,
+            series_id: seriesId,
+            series_number: data.seriesNumber ?? null,
+            themes: data.themes ?? null
+        })
+        .select("id")
+        .single();
+
+    if(insertError){
+        throw insertError;
+    }
+
+    return newBook.id;
 }
