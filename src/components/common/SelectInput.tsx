361068d5ed3f5de8d5aa52aee48703e5fd3aace1
Anna Schiavo <anna.schiavo2006@gmail.com>
2026-07-31 17:07:06 +0200

    Created SelectInput.css

--- src/components/common/SelectInput.tsx
+++ src/components/common/SelectInput.tsx
@@ -1,63 +1,147 @@
-import { forwardRef } from "react";
-import "./Input.css";
-import type {
-    ChangeEventHandler,
-    FocusEventHandler
+import {
+    useState
 } from "react";
+import "./SelectInput.css";
 
 export interface SelectOption {
     id: string;
     name: string;
 }
 
-
 interface SelectInputProps {
     label: string;
     options: SelectOption[];
-    required?: boolean;
-    name?: string;
-    onChange?: ChangeEventHandler<HTMLSelectElement>;
-    onBlur?: FocusEventHandler<HTMLSelectElement>;
+    value?: string;
+    onChange: (value: string) => void;
 }
 
-const SelectInput = forwardRef<HTMLSelectElement, SelectInputProps>(
-    function SelectInput({
-        label,
-        options,
-        required = false,
-        name,
-        onChange,
-        onBlur
-    }, ref) {
+export default function SelectInput({
+                                        label,
+                                        options,
+                                        value,
+                                        onChange
+                                    }: SelectInputProps) {
 
-        return (
-            <div>
-                <label>{label}</label>
+    const [open, setOpen] = useState(false);
 
-                <select
-                    ref={ref}
-                    name={name}
-                    required={required}
-                    onChange={onChange}
-                    onBlur={onBlur}
-                >
-                    <option value="">
-                        Select...
-                    </option>
 
-                    {options.map(option => (
-                            <option
-                                key={option.id}
-                                value={option.id}
-                            >
-                                {option.name}
-                            </option>
-                        ))
-                    }
-                </select>
-            </div>
-        );
+    const selectedOption = options.find(
+        option => option.id === value
+    );
+
+    function selectOption(option: SelectOption) {
+        onChange(option.id);
+        setOpen(false);
     }
-);
 
-export default SelectInput;
+    return (
+        <div className="select-input">
+            <label>
+                {label}
+            </label>
+
+            <div
+                className="select-box"
+                onClick={() =>
+                    setOpen(!open)
+                }
+            >
+                <span>
+                    {
+                        selectedOption
+                            ? selectedOption.name
+                            : "Select..."
+                    }
+                </span>
+
+                <span>
+                    ▾
+                </span>
+            </div>
+
+            {
+                open && (
+                    <ul className="select-list">
+                        {
+                            options.map(option => (
+                                <li
+                                    key={option.id}
+                                    className="select-item"
+                                    onClick={() =>
+                                        selectOption(option)
+                                    }
+                                >
+                                    {option.name}
+                                </li>
+                            ))
+                        }
+                    </ul>
+                )
+            }
+        </div>
+    );
+}
+
+// import { forwardRef } from "react";
+// import "./Input.css";
+// import type {
+//     ChangeEventHandler,
+//     FocusEventHandler
+// } from "react";
+//
+// export interface SelectOption {
+//     id: string;
+//     name: string;
+// }
+//
+//
+// interface SelectInputProps {
+//     label: string;
+//     options: SelectOption[];
+//     required?: boolean;
+//     name?: string;
+//     onChange?: ChangeEventHandler<HTMLSelectElement>;
+//     onBlur?: FocusEventHandler<HTMLSelectElement>;
+// }
+//
+// const SelectInput = forwardRef<HTMLSelectElement, SelectInputProps>(
+//     function SelectInput({
+//         label,
+//         options,
+//         required = false,
+//         name,
+//         onChange,
+//         onBlur
+//     }, ref) {
+//
+//         return (
+//             <div>
+//                 <label>{label}</label>
+//
+//                 <select
+//                     ref={ref}
+//                     name={name}
+//                     required={required}
+//                     onChange={onChange}
+//                     onBlur={onBlur}
+//                 >
+//                     <option value="">
+//                         Select...
+//                     </option>
+//
+//                     {options.map(option => (
+//                             <option
+//                                 key={option.id}
+//                                 value={option.id}
+//                             >
+//                                 {option.name}
+//                             </option>
+//                         ))
+//                     }
+//                 </select>
+//             </div>
+//         );
+//     }
+// );
+//
+// export default SelectInput;
