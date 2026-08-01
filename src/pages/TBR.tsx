c50d3ae310c9089a83c1d2c31b2b7e351cdc2596
Anna Schiavo <anna.schiavo2006@gmail.com>
2026-08-01 12:15:49 +0200

    Table for TBR page

--- src/pages/TBR.tsx
+++ src/pages/TBR.tsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
+
+import WishlistTable from "../components/WishlistTable";
+import type { WishlistWithDetails } from "../types/WishlistWithDetails";
+
+import { getWishlist } from "../services/wishlistService";
+
+
 function TBR() {
+    const [wishlist, setWishlist] = useState<WishlistWithDetails[]>([]);
+
+    useEffect(() => {
+        async function loadWishlist() {
+            try {
+                const data = await getWishlist();
+                setWishlist(data);
+            } catch(error) {
+                console.error(
+                    "Error loading wishlist:",
+                    error
+                );
+            }
+        }
+
+        loadWishlist();
+    }, []);
+
     return (
         <div>
             <h1>TBR</h1>
+            <WishlistTable
+                wishlist={wishlist}
+            />
         </div>
-    )
+    );
 }
-
-export default TBR
+export default TBR;
