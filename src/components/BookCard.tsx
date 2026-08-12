e5170c762f5a30c4019a4d39fcf5c549a45c313e
Anna Schiavo <anna.schiavo2006@gmail.com>
2026-08-12 10:29:06 +0200

    Book Card connected to ReadingDetails page

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import type { ReadingWithDetails } from "../types/ReadingWithDetails.ts";
 import "./BookCard.css"
 
@@ -6,14 +7,25 @@ interface BookCardProps {
 }
 
 function BookCard({reading}: BookCardProps) {
+    const navigate = useNavigate();
     const book = reading.book;
 
     return (
-        <div className="book-card">
+        <div className="book-card"
+            onClick={() => navigate(`/readings/${reading.id}`)}
+            role="button"
+            tabIndex={0}
+            onKeyDown={(event) => {
+                if (event.key === "Enter") {
+                    navigate(`/readings/${reading.id}`);
+                }
+            }}
+        >
             <img
                 src={reading.cover_url ?? ""}
                 alt={book.title}
             />
+
             <div>
                 <h3>{book.title}</h3>
                 <p>{book.author.name}</p>
